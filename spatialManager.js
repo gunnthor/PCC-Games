9dc73aed13e1dd9592328a45ce0e6da5cd3b7aaf
Notandi <Notandi@users.noreply.github.com>
2015-11-04 12:28:49 +0000

    buggy as fuck

--- spatialManager.js
+++ spatialManager.js
@@ -107,24 +107,42 @@ findEntityInRange: function(posX, posY, width, height, colEntity) {
     }*/
 
     // variables for all the sides of the object 
+
     var dispX = colEntity.velX;
     var dispY = colEntity.velY;
+    var right = colEntity.cx + width;
+    var left = colEntity.cx - width;
+    var top = colEntity.cy - height;
+    var bottom = colEntity.cy + height;
+    var rightWrapped = util.wrapX(right);
+    var leftWrapped = util.wrapX(left);
+    var topWrapped = util.wrapY(top);
+    var bottomWrapped = util.wrapY(bottom);
 
-    var right = util.wrapX(colEntity.cx + colEntity.width/2);
-    var prevRight = right - dispX;
+    //Tékka hveru margir kassar verða til útfrá wrap 1 til 3 auka collision check eftir því hve margir wrap kassar verða til  annars bara þetta eina 
+    if (util.isBetween(top,0,g_canvas.height) || util.isBetween(bottomm,0,g_canvas.height)){
+        if(util.isBetween(right,0,g_canvas.width) || util.isBetween(left,0,g_canvas.width)){
+            // Það þarf að collision checka á fjórum kössum 
+            this.findEntityInRangeCollision();
 
-    var left = util.wrapX(colEntity.cx - colEntity.width/2);
-    var prevLeft = left - dispX;
+        }
+        else{
+            // það þarf bara að checka á tveimur top og bottom kössum 
 
-    var bottom = util.wrapX(colEntity.cy + colEntity.height/2);
-    var prevBottom = bottom - dispY;
+        }
+    }
+    else if(util.isBetween(right,0,g_canvas.width) || util.isBetween(left,0,g_canvas.width)){
+        // það þarf að cehcka á tveimur left og right kössum
 
-    var top = util.wrapX(colEntity.cy - colEntity.height/2);
-    var prevTop = bottom - dispY;
-    
-    // Check against all entitys
+    }
+    else{
+        //það þarf bara að checka á kassanum eins og hann er 
+        this.findEntityInRangeCollision(right,left,top,bottom,colEntity);
+    }
 
-    for(var i = 1; i < this._entities.length; i++) {
+},
+findEntityInRangeCollision: function(right,left,top,bottom,colEntity){
+     for(var i = 1; i < this._entities.length; i++) {
         
         //if(colEntity.getSpatialID() === i) continue;
 
@@ -150,37 +168,31 @@ findEntityInRange: function(posX, posY, width, height, colEntity) {
         // Check for x coords
 
         if(right > entLeft &&
-            right - width < entRight) {
+            left < entRight) {
             
             // Check for y coords on top collision
             if(prevBottom <= entTop &&
                 bottom >= entTop) {
-                    colEntity.cy = entity.cy - entity.height/2 - colEntity.height/2;
-                    colEntity.velY = 0;
-                    colEntity.IN_AIR = false;
-                    //colEntity.SLOWING_DOWN = tru
-        }
-        if(left > entLeft &&
-            left + width < entRight) {
-            
-            // Check for y coords on top collision
-            if(prevBottom <= entTop &&
-                bottom >= entTop) {
-    
+                    
+                // Do this
+                if(Math.abs(colEntity.cy - colEntity.prevCy) <= colEntity.velYLimit*2) {
                     colEntity.cy = entity.cy - entity.height/2 - colEntity.height/2;
                     colEntity.velY = 0;
                     colEntity.IN_AIR = false;
                     //colEntity.SLOWING_DOWN = true;
-            }    
+                }
+            }
 
             // Check for y coords on bottom collision
             else if(prevTop >= entBottom &&
                     top <= entBottom) {
                         
-                
+                // Do this
+                if(Math.abs(colEntity.cy - colEntity.prevCy) <= colEntity.velYLimit*2) {
                     colEntity.cy = entity.cy + entity.height/2 + colEntity.height/2;
                     colEntity.velY = 0;
 
+                }
             }
         }
 
@@ -190,67 +202,34 @@ findEntityInRange: function(posX, posY, width, height, colEntity) {
 
         // Check for y coords
         if(bottom > entTop &&
-            bottom - height < entBottom) {
+            top < entBottom) {
             
             // Check for x coords on left collision
             if(prevRight <= entLeft &&
                 right >= entLeft) {
                 
-                
+                // Do this
+                if(Math.abs(colEntity.cx - colEntity.prevCx) <= colEntity.velXLimit*2) {
                     colEntity.cx = entity.cx - entity.width/2 - colEntity.width/2;
                     colEntity.velX = 0;
-                
+                }
             }
 
             // Check for x coords on right collision
             else if(prevLeft >= entRight &&
                     left <= entRight) {
                      
-                
+                // Do this
+                if(Math.abs(colEntity.cx - colEntity.prevCx) <= colEntity.velXLimit*2) {
                     colEntity.cx = entity.cx + entity.width/2 + colEntity.width/2;
                     colEntity.velX = 0;
-                
-            }
-        }
-        if(top > entTop &&
-            top + height < entBottom) {
-            
-            // Check for x coords on left collision
-            if(prevRight <= entLeft &&
-                right >= entLeft) {
-                
-                
-                    colEntity.cx = entity.cx - entity.width/2 - colEntity.width/2;
-                    colEntity.velX = 0;
-                
-            }
-
-            // Check for x coords on right collision
-            else if(prevLeft >= entRight &&
-                    left <= entRight) {
-                     
-                
-                    colEntity.cx = entity.cx + entity.width/2 + colEntity.width/2;
-                    colEntity.velX = 0;
-                
+                }
             }
         }
         
     }
 
-    // ÞARF AÐ BREYTA ÞESSU FYRST VIÐ VERÐUM MEÐ KASSA HIT BOX
-    /*for (var ID in this._entities) {
-        var e = this._entities[ID];
-        if( e.isUndefined) continue;
-        var distsq = util.wrappedDistSq(posX,posY,e.posX,e.posY,g_canvas.width,g_canvas.height);
-        var radiussq = (radius + e.radius) * (radius + e.radius);
-        if ( distsq < radiussq){
-            return e.entity;
-        }
-    }*/
-
 },
-
 render: function(ctx) {
     var oldStyle = ctx.strokeStyle;
     var oldFill = ctx.fillStyle;
