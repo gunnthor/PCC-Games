ced99addcffd0cb6cf4551f60f0ff3b002c0125e
Notandi <oskarola7@gmail.com>
2015-11-05 19:02:41 +0000

    1

--- spatialManager.js
+++ spatialManager.js
@@ -92,7 +92,6 @@ unregister: function(entity)
     entity.lastCollision = undefined;
     
 },
-
 findEntityInRange: function(posX, posY, width, height, colEntity) {
 
     // TODO: YOUR STUFF HERE!
@@ -105,6 +104,143 @@ findEntityInRange: function(posX, posY, width, height, colEntity) {
             posY  - height/2 < e.posY - e.height/2 + e.height &&
             posY  - height/2 + height > e.posY - e.height/2) return e.entity;
     }*/
+
+    // variables for all the sides of the object 
+    var dispy = colEntity.cy - colEntity.prevCy;
+    var dispx = colEntity.cx - colEntity.prevCx;
+    console.log(dispy);
+
+    var right = util.wrapX(colEntity.cx + colEntity.width/2);
+    var prevRight = right - dispx;
+
+    var left = util.wrapX(colEntity.cx - colEntity.width/2);
+    var prevLeft = left - dispx;
+
+    var bottom = util.wrapY(colEntity.cy + colEntity.height/2);
+    var prevBottom = bottom - dispy;
+
+    var top = util.wrapY(colEntity.cy - colEntity.height/2);
+    var prevTop = top - dispy;
+    
+    // Check against all entitys
+
+    for(var i = 1; i < this._entities.length; i++) {
+        
+        //if(colEntity.getSpatialID() === i) continue;
+
+        // Get an object with the entity coords and size
+        var entity = this._entities[i];
+
+        if (entity.isUndefined) continue;
+
+
+        //variables for all the sides of the entities beign checked
+
+        var entRight = entity.cx + entity.width/2;
+
+        var entLeft = entity.cx - entity.width/2;
+
+        var entTop =  entity.cy - entity.height/2;
+
+        var entBottom = entity.cy + entity.height/2;
+
+
+        // Collision with the Top and Bottom of a entity
+ 
+        if(right > entLeft &&
+            right - width < entRight){
+
+            if(prevBottom <= entTop &&
+                bottom >= entTop) {
+
+                colEntity.cy = entity.cy - entity.height/2 - colEntity.height/2;
+                colEntity.velY = 0;
+                colEntity.IN_AIR = false;
+                console.log("top");
+
+            }
+
+            else if(prevTop >= entBottom &&
+                    top <= entBottom) {
+
+                colEntity.cy = entity.cy + entity.height/2 + colEntity.height/2;
+                colEntity.velY = 0;
+                console.log("bottom");
+
+            } 
+        }
+        if(left + width > entLeft &&
+            left < entRight){
+
+            if(prevBottom <= entTop &&
+                bottom >= entTop) {
+
+                colEntity.cy = entity.cy - entity.height/2 - colEntity.height/2;
+                colEntity.velY = 0;
+                colEntity.IN_AIR = false;
+                console.log("top");
+
+            }
+
+            else if(prevTop >= entBottom &&
+                    top <= entBottom) {
+
+                colEntity.cy = entity.cy + entity.height/2 + colEntity.height/2;
+                colEntity.velY = 0;
+                console.log("bottom");
+                
+            } 
+        }
+       
+       if (bottom > entTop &&
+            bottom - height < entBottom){
+            if(prevRight <= entLeft &&
+                right >= entLeft) {
+                colEntity.cx = entity.cx - entity.width/2 - colEntity.width/2;
+                colEntity.velX = 0;
+                console.log("left");
+            }
+            else if(prevLeft >= entRight &&
+                    left <= entRight){
+                colEntity.cx = entity.cx + entity.width/2 + colEntity.width/2;
+                colEntity.velX = 0;
+                console.log("right");
+
+            }
+       }
+       if (top + height > entTop &&
+            top < entBottom){
+            if(prevRight <= entLeft &&
+                right >= entLeft) {
+                colEntity.cx = entity.cx - entity.width/2 - colEntity.width/2;
+                colEntity.velX = 0;
+                console.log("left");
+            }
+            else if(prevLeft >= entRight &&
+                    left <= entRight){
+                colEntity.cx = entity.cx + entity.width/2 + colEntity.width/2;
+                colEntity.velX = 0;
+                console.log("right");
+
+            }
+       }
+}
+
+
+    // ÞARF AÐ BREYTA ÞESSU FYRST VIÐ VERÐUM MEÐ KASSA HIT BOX
+    /*for (var ID in this._entities) {
+        var e = this._entities[ID];
+        if( e.isUndefined) continue;
+        var distsq = util.wrappedDistSq(posX,posY,e.posX,e.posY,g_canvas.width,g_canvas.height);
+        var radiussq = (radius + e.radius) * (radius + e.radius);
+        if ( distsq < radiussq){
+            return e.entity;
+        }
+    }*/
+
+},
+/*
+findEntityInRange: function(posX, posY, width, height, colEntity) {
 
     // variables for all the sides of the object 
     var right = colEntity.cx + colEntity.width/2 ;
@@ -207,19 +343,7 @@ findEntityInRange: function(posX, posY, width, height, colEntity) {
         }
         
     }
-
-    // ÞARF AÐ BREYTA ÞESSU FYRST VIÐ VERÐUM MEÐ KASSA HIT BOX
-    /*for (var ID in this._entities) {
-        var e = this._entities[ID];
-        if( e.isUndefined) continue;
-        var distsq = util.wrappedDistSq(posX,posY,e.posX,e.posY,g_canvas.width,g_canvas.height);
-        var radiussq = (radius + e.radius) * (radius + e.radius);
-        if ( distsq < radiussq){
-            return e.entity;
-        }
-    }*/
-
-},
+},*/
 
 render: function(ctx) {
     var oldStyle = ctx.strokeStyle;
