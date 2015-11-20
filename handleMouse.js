08bf336d81c0ae83ee184e52ccd0e7859a8203a5
Notandi <Notandi@users.noreply.github.com>
2015-11-20 23:45:33 +0000

    fixxaði mouse

--- handleMouse.js
+++ handleMouse.js
@@ -19,14 +19,16 @@ function handleMouse(evt) {
     g_mouseX = evt.clientX - g_canvas.offsetLeft;
     g_mouseY = evt.clientY - g_canvas.offsetTop;
     
-    // If no button is being pressed, then bail
-    var button = evt.buttons === undefined ? evt.which : evt.buttons;
-    if(!g_menu) return;
     Menu.onhover(g_mouseX,g_mouseY);
-    if (!button) return;
+   
+}
+function handleMousedown(evt) {
+    
+    g_mouseX = evt.clientX - g_canvas.offsetLeft;
+    g_mouseY = evt.clientY - g_canvas.offsetTop;
     Menu.onclick(g_mouseX,g_mouseY);
 }
 
 // Handle "down" and "move" events the same way.
-window.addEventListener("mousedown", handleMouse);
+window.addEventListener("mousedown", handleMousedown);
 window.addEventListener("mousemove", handleMouse);
